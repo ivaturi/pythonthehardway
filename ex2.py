29ecbde0d03374a5c3161caee438de43c4310fa0
abhi <abhi@mailbox.org>
2016-08-04 02:20:39 +0200

    exercise 2

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# this is a comment
+# python ignores everything after the hash
+
+print "I could have code like this." # and the comment after is ignored
+
+# print "This won't run"
+
+print "This will run"
