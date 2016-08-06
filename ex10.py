32ea6fa31285811a01252449ea99533f7b396200
abhi <abhi@mailbox.org>
2016-08-06 11:59:21 +0200

    exercise 10: what was that?

---
+++ ex10.py
@@ -0,0 +1,27 @@
+# exercise 10: understanding exercise 9, more printing
+
+# \t introduces a tab
+tabby_cat = "\tI'm tabbed in."
+
+# \n introduces a new line
+persian_cat = "I'm split\non a line."
+
+# \\ introduces the backslash
+backslash_cat =  "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+# random
+while True:
+	for i in ["/","-","|","\\","|"]:
+		print "%s\r" % i,
