84f96c4d9da8b4f3748002aff4fdad26ec2cdc94
abhi <abhi@mailbox.org>
2016-08-07 07:35:05 +0200

    exercise 11: aking questions

---
+++ ex11.py
@@ -0,0 +1,12 @@
+# exercise 11: asking questions
+
+print "how old are you?",
+age = raw_input()
+
+print "how tall are you?",
+height = raw_input()
+
+print "How much do you weigh?",
+weight = int(raw_input())
+
+print "So, you're %s old, %s tall, and %d heavy" % (age, height, weight)
