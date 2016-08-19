4721e03ef1c616e9befdf480f525febead32b6f9
abhi <abhi@mailbox.org>
2016-08-19 07:01:11 +0200

    exercise 32: loops and lists, study drill 3

--- ex32.py
+++ ex32.py
@@ -33,7 +33,7 @@ for element in elements:
     print "Element was: %d" % element
 
 
-### Study drills ###
+### Study drill  - 2 ###
 
 # What type is a range?
 print "range(0,6) is of type *%s*" % type(range(0,6))
@@ -41,3 +41,35 @@ print "range(0,6) is of type *%s*" % type(range(0,6))
 # Assign a range to a list
 range_list = range(0,6)
 print range_list
+
+### Study drill  - 3 ###
+
+# What things can we do with lists?
+# Quite a few, according to help(list)...
+
+my_list = ["first_is", "This", "is", "list", "is"]
+print my_list
+
+# we can count the occurrences of an item
+print "'is' occurs %d times in my_list" % my_list.count("is")
+
+# we can pop values (default: last, but we can specify an index)
+print "Popping from the list: %s" % my_list.pop()
+print "Popping from the beginning : %s" % my_list.pop(0)
+print my_list
+
+# we can insert values:
+my_list.insert(2,"a")
+print my_list
+
+# we can extend a list
+my_list.extend([1,2,3,4,5])
+print my_list
+
+# we can reverse a list
+my_list.reverse()
+print my_list
+
+# we can sort the items in a list
+my_list.sort()
+print my_list
