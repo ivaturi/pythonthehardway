75b22d2a3aa86dab350cda2d3eaf581ca0f69fdf
abhi <abhi@mailbox.org>
2016-08-20 12:45:41 +0200

    exercise 34: accessing elements of lists

---
+++ ex34.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+# Exercise 34: Accessing elements of lists
+
+# let's create a list of animals:
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# accessing the elements:
+
+print animals
+print "The animal at 1 is %r" % animals[1]
+print "The third animal is %r" % animals[2]
+print "The first animal is %r" % animals[0]
+print "The animal at 3 is %r" % animals[3]
+print "The fifth animal is %r" % animals[4]
+print "The animal at 2 is %r" % animals[2]
+print "The sixth animal is %r" % animals[5]
+print "The animal at 4 is %r"  % animals[4]
