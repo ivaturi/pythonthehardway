da1ff618c3a8ea6e24822263298dc912bc735adb
abhi <abhi@mailbox.org>
2016-08-20 14:57:46 +0200

    exercise 36: debugging and designing - rules

---
+++ ex36.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+# Rules for if-statements:
+#  - Every if statement must have an else
+#  - In else-statements that 'should never run', use die() 
+#  - Never nest if-statements more than two deep 
+#  - Blank lines before and after if-elif-else statements
+#  - Move complex boolean calculations outside if-checks
+
+
+# Rules for loops:
+#  - Try and never use while-loops
+#  - Use for-loops when there are a fixed or limited number of things to loop over
+
+# Debugging
+#  - Use print statements to check what is happening in the code 
