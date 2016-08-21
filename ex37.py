7f25da49465aafbbd72024fc46182719966e6487
abhi <abhi@mailbox.org>
2016-08-21 18:49:54 +0200

    exercise 37: logical operators and boolean data types

---
+++ ex37.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+# Exercise 37: symbol review
+
+## Keywords:
+print "-- Keywords (logical operators and booleans) --"
+print "True and False is >>> ", True and False
+print "True or False is >>> ", True or False
+print "True and not False is >>> ", True and not False
+
+print "-- Keywords (conditionals --)"
