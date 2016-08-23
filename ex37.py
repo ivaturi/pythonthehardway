ef75dcdd851f55c0297b0647ce9bdd8127c24a1b
abhi <abhi@mailbox.org>
2016-08-23 19:13:58 +0200

    exercise 37: notes on exceptions

--- ex37.py
+++ ex37.py
@@ -99,3 +99,23 @@ def test_assert(x,y):
 
 test_assert(1, 2.2)
 #test_assert(2.2, 1)  # <--- This will throw an AssertionError, because the first input is not an integer     
+
+
+
+##### TRY-EXCEPT-FINALLY #####
+
+#  The try statement is used to specify exception handlers.
+#  An exception is a point in the program where normal flow
+#  of control breaks. A 'try' block helps us handle these
+#  exceptions by providing exception handlers (except).
+
+#  An exception raised by a 'try' block should be handled by
+#  a corresponding 'except' block. The except block can either
+#  handle specific kinds of exceptions (TypeError/DivideByZeroError)
+#  or it can just catch all generic exceptions (not a very good idea)
+
+#  If a try-except block is followed by a 'finally' block. the 'finally'
+#  block is executed *no matter what happens* in the try block.
+
+# If an exception is raised and isn't caught by any 'except' statement
+#  the program is terminated.
