213cbe40642aa3199ede33987f289c6158533723
abhi <abhi@mailbox.org>
2016-08-24 23:19:47 +0200

    exercise 37: lambdas

--- ex37.py
+++ ex37.py
@@ -135,3 +135,18 @@ finally:
     a = 1/2.0
 
 print "a = %f" % a
+
+
+##### LAMBDA #####
+
+print "\n"
+print "-- lambda --"
+
+def multiplier(x):
+    return lambda n: n * x
+
+times3 = multiplier(3)
+times20 = multiplier(20)
+
+print "4 times 3 is %d" % times3(4)
+print "4 times 20 is %d" % times20(4)
