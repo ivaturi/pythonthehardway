0ef323dc7d826ef293225a62350648aa9e328c91
abhi <abhi@mailbox.org>
2016-08-26 10:03:42 +0200

    exercise 37: more on lambdas

--- ex37/lambda.py
+++ ex37/lambda.py
@@ -1,7 +1,16 @@
 #! /usr/bin/python
 
-##### LAMBDA #####
-
+# Lambdas
+# ------
+# A lambda (or an anonymous function) is a function that is not bound to any identifier
+# Lambdas are often used when we want to return a function as an argument
+#
+# Unlike in other functional programming languages, python does not let us define
+# normal functions anonymously; a lambda can only contain an <expression> that is returned
+#
+# A lambda function can be used anywhere a function is expected.
+#
+#
 print "\n"
 print "-- lambda --"
 
@@ -13,3 +22,23 @@ times20 = multiplier(20)
 
 print "4 times 3 is %d" % times3(4)
 print "4 times 20 is %d" % times20(4)
+
+
+# Slightly advanced use-cases:
+#
+# Lamda functions are useful in list operations...
+
+my_array = [10,15,20,23, 26, 27, 13, 29, 43, 56, 108]
+print my_array
+
+# retrieve only the elements that are divisible by 5:
+print "Divisible by 5:"
+print filter(lambda x:x%5==0, my_array)
+
+# square each element of the array
+print "Squared:"
+print map(lambda x: x**2, my_array)
+
+# compute the sum of the elements of the array
+print "Sum"
+print reduce(lambda x,y: x+y, my_array)
