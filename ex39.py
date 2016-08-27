9216af697f216064ea121efe9f695e2e8f262d1c
abhi <abhi@mailbox.org>
2016-08-28 00:25:47 +0200

    exercise 39: dictionary operations - numerical indices and deleting items

--- ex39.py
+++ ex39.py
@@ -8,13 +8,29 @@ profile = {'name' : 'Matthew Murdock ', 'profession': 'Lawyer', 'alias': 'Darede
 
 print "profile: ", profile
 
+print "------------------"
+print "Numerical indices!"
+print "------------------"
+
+# We can also add numerical indices:
+profile[1] = "Superheroes!"
+print "Updated profile: ", profile
+
 
 print "------------------"
 print "Accessing elements"
 print "------------------"
 
+print "           [1] : ", profile[1]
 print "        [name] : ", profile['name']
 print "       [alias] : ", profile['alias']
 print "    [location] : ", profile['location']
 print "  [profession] : ", profile['profession']
 
+print "-----------------"
+print "Deleting elements"
+print "-----------------"
+
+del profile[1]
+del profile['alias']
+print "Updated profile: ", profile
