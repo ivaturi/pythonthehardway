cfb83a5ee0608865d983ca38f4bc65afb4f08f66
abhi <abhi@mailbox.org>
2016-08-27 23:10:44 +0200

    Exercise 39: Dictionaries

---
+++ ex39.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+# Exercise 39: Dictionaries, oh lovely dictionaries!
+
+# A dictionary associates one thing to another, no matter what it is
+
+profile = {'name' : 'Matthew Murdock ', 'profession': 'Lawyer', 'alias': 'Daredevil', 'location': 'Hell\'s Kitchen'}
+
+print "profile: ", profile
+
+
+print "------------------"
+print "Accessing elements"
+print "------------------"
+
+print "        [name] : ", profile['name']
+print "       [alias] : ", profile['alias']
+print "    [location] : ", profile['location']
+print "  [profession] : ", profile['profession']
+
