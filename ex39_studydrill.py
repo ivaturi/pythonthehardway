60f00f2e3f37907ae128a20e85996ba71d78aa2c
abhi <abhi@mailbox.org>
2016-08-29 08:39:13 +0200

    exercise 39: initializing dictionaries

--- ex39_studydrill.py
+++ ex39_studydrill.py
@@ -1,25 +1,32 @@
 #! /usr/bin/env python
 
-# Exercise 39: Notes on dictionaries
+## Exercise 39: Notes on dictionaries
 
-###
-# A dictionary is an associative array that maps hashable values to objects
-# The keys of a dictionary should be hashable
-# (the immutable basic types that are supported by python)
+##
+##
+## A dictionary is an associative array that maps hashable values to objects
+## The keys of a dictionary should be hashable
+## (the immutable basic types that are supported by python)
+##
+##
 
+#
+# Different ways to create an empty dictionary
+# --------------------------------------------
+#
 
-# Different ways to create an empty  dictionary:
-#----------------------------------------------
-
-my_dict  = {}
-my_dict2 = dict()
+my_dict  =     {}  # <<< this is more common
+my_dict2 = dict()  # <<< this is slower 
 
 # a little more complicated (probably unnecessary):
 import types
 my_dict3 = types.DictType.__new__(types.DictType, (), {})
 
 
-
+#
+# Different ways to initialize a non-empty dictionary
+# ---------------------------------------------------
+#
 
 # Using key-value pairs
 westeros  = {
@@ -30,5 +37,12 @@ westeros  = {
 }
 print "westeros: ", westeros
 
+# Using the dict constructor
+lords = dict(
+    lannister = 'tywin',
+        stark = 'ned',
+      greyjoy = 'balon',
+    baratheon = 'robert'
+)
+print "lords: ", lords
 
-#
