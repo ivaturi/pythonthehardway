295d9d45899cc1ce2b599fb0bb74575ec1349a29
abhi <abhi@mailbox.org>
2016-08-31 22:56:07 +0200

    exercise 41: getting started

---
+++ ex41/oop_test.py
@@ -0,0 +1,13 @@
+
+#! /usr/bin/env python
+
+# Exercise 41: Learning to speak object-oriented
+
+import random
+from urllib import urlopen
+import sys
+
+
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = []
