45861b332c2b5137c6d5433cdabd35300c55d0c3
abhi <abhi@mailbox.org>
2016-09-01 17:44:11 +0200

    exercise 41: phrase drills

---
+++ ex41/phrase_drills.py
@@ -0,0 +1,39 @@
+# Exercise 41: Phrase drills
+
+
+
+#Make a class named X that is-a Y.
+class X(Y)
+#----------------------------------------------------------------------------
+
+
+
+#Class X 'has-a' __init__ that takes two parameters, <self> and <J>.
+class X(object):
+    def __init__(self, J)
+#----------------------------------------------------------------------------
+
+
+
+#Class X has a function named M that takes two parameters: <self> and <J>
+class X(object):
+    def M(self, J)
+#----------------------------------------------------------------------------
+
+
+
+#Set foo to an instance of class X
+foo = X()
+#----------------------------------------------------------------------------
+
+
+
+#From foo get the M function, and call it with parameters self, J
+foo.M(J)
+#----------------------------------------------------------------------------
+
+
+
+#From foo get the K attribute and set it to Q
+foo.K = Q
+#----------------------------------------------------------------------------
