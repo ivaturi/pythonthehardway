2179245c25c888ec6cc59a95817e8f1886b06eaf
abhi <abhi@mailbox.org>
2016-09-01 08:45:16 +0200

    exercise 41: word drills

---
+++ ex41/word_drills.py
@@ -0,0 +1,28 @@
+#
+# Word drills for exercise 41
+# ---------------------------
+#
+
+
+# class
+#     - Tells Python to make a new type of thing
+#
+# -----------------------------
+#
+# object
+#    - This is the most basic type of thing
+#    - This also refered to an instance of a class (some thing)
+#
+# -----------------------------
+#
+# instance
+#    - The result of telling Python to create a class.
+#
+# -----------------------------
+#
+# def
+#    - Used to define functions
+#
+# -----------------------------
+#
+#
