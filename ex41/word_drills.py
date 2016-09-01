4dc03ef00551d2299a4bc899bc5d6498de276b05
abhi <abhi@mailbox.org>
2016-09-01 15:43:45 +0200

    exercise 41: more terms

--- ex41/word_drills.py
+++ ex41/word_drills.py
@@ -25,4 +25,33 @@
 #
 # -----------------------------
 #
+# self
+#    - A variable for the instance or the object being accessed
 #
+# -----------------------------
+#
+# inheritance
+#    - the idea that one thing (a class) can inherit properties
+#      (variables/functions/etc.) from another thing (a class)
+#
+# -----------------------------
+#
+# composition
+#    - the idea that a class can be composed of other classes
+#
+# -----------------------------
+#
+# attribute
+#    - properties (usually variables) that classes have, from composition
+#
+# -----------------------------
+#
+# is-a
+#    - a phrase used to indicate inheritance (a bike 'is a' two-wheeler)
+#
+# -----------------------------
+#
+# has-a
+#    - a phrase used to indicate composition (a two-wheeler 'has' two wheels)
+#
+# -----------------------------
