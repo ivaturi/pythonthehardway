0a9c9cf97c35d1c722edc708edfaf9295aad27c1
abhi <abhi@mailbox.org>
2016-09-02 23:45:22 +0200

    exercise 41: added phrases to oop_test

--- ex41/oop_test.py
+++ ex41/oop_test.py
@@ -11,3 +11,20 @@ import sys
 
 WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
+
+PHRASES = {
+    "class %%%(%%%):":
+        "Make a class names %%% that is-a %%%.",
+    "class %%%(object):\n\tdef __init__(self,***)":
+        "class %%% has-a __init__ that takes self and *** parameters.",
+    "class %%%(object):\n\tdef ***(self,@@@)":
+        "class %%% has-a function names *** that takes self and @@@ parameters.",
+    "*** = %%%()":
+        "Set *** to an instance of class %%%",
+    "***.***(@@@)":
+        "From *** get the *** function, and call it with parameters self, @@@.",
+    "***.***" = '***'"":
+        "From *** get the *** attribute and set it to '***'."
+}
+
+
