5f08a3358d54998451f966640ad520699ae9e63e
abhi <abhi@mailbox.org>
2016-09-05 22:29:16 +0200

    exercise 42: a basic game engine...the beginnin

---
+++ ex42/game.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+# A simple game engine
+
+class Scene(object):
+    def enter(self):
+        pass
+
+class Engine(object):
+    def __init__(self, scene_map):
+        pass
+
+    def play(self):
+        pass
+
+
+class Death(scene):
+    def enter(self):
+        pass
+
+
+class CentralCorridor(Scene):
+    def enter(self):
+        pass
+
+class LaserWeaponArmory(Scene):
+    def enter(self):
+        pass
+
+class TheBridge(Scene):
+    def enter(self):
+        pass
+
+class EscapePod(Scene):
+    def enter(self):
+        pass
+
